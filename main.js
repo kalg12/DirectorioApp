3587c637437c374babba96c7a769d1fb1ccbbbfd
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-05-26 10:34:27 -0600

    Nuevas implementaciones

--- main.js
+++ main.js
@@ -10,49 +10,27 @@ const consumirApi = () => {
     .then((data) => {
       //La data de la api la vamos a llevar al HTML
       //Paso 1.- Definimos las constantes que vamos a usar y el HTML que vamos a afectar
-      const nombre = document.getElementById("nombre");
-      const nombreUsuario = document.getElementById("nombreUsuario");
-      const email = document.getElementById("email");
-      const phone = document.getElementById("phone");
-      const website = document.getElementById("website");
-      const street = document.getElementById("street");
       const contenedor = document.getElementById("contenedor");
 
-      nombre.innerText = data[6].name;
-      nombreUsuario.innerText = data[6].username;
-      email.innerText = data[6].email;
-      phone.innerText = data[6].phone;
-      website.innerText = data[6].website;
-      street.innerHTML = `<h1>${data[6].address.street}</h1>`;
-
-      /* Persona 2 */
-      const nombreDos = document.getElementById("nombreDos");
-      const nombreUsuarioDos = document.getElementById("nombreUsuarioDos");
-      const emailDos = document.getElementById("emailDos");
-      const phoneDos = document.getElementById("phoneDos");
-      const websiteDos = document.getElementById("websiteDos");
-      const streetDos = document.getElementById("streetDos");
-
-      nombreDos.innerText = data[5].name;
-      nombreUsuarioDos.innerText = data[5].username;
-      emailDos.innerText = data[5].email;
-      phoneDos.innerText = data[5].phone;
-      websiteDos.innerText = data[5].website;
-      streetDos.innerHTML = `<h1>${data[5].address.street}</h1>`;
-
       /* Aquí vamos a facilitarnos la vida con ForEach */
-      console.log(data);
-
       data.forEach(
-        (personita, index) =>
+        (personita) =>
           (contenedor.innerHTML += `
-            <p>${personita.name}</p>
-            <p>${personita.username}</p>
-            <p>${personita.email}</p>
-            <p>${personita.phone}</p>
-            <p>${personita.website}</p>
-            <hr>
-            `)
+
+<div class="card" style="width: 18rem;">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png" class="card-img-top" alt="usuario">
+  <div class="card-body">
+    <h5 class="card-title">${personita.name}</h5>
+    <p class="card-text">Información de contacto de la persona</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Correo electrónico:${personita.email}</li>
+    <li class="list-group-item">Calle:${personita.address.street}</li>
+    <li class="list-group-item">Departamento:${personita.address.suite}</li>
+  </ul>
+</div>
+
+      `)
       );
     })
     //Descubrir que hacer en caso de que no me corresponda
